a6f711a8d59eab82c50f2bfa2c970634da4db150
Michel Beloshitsky <itano@yandex.ru>
2022-05-10 00:39:10 +0300

    prettier fixes

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import { Blob, BlobHeader} from './fileformat';
+import { Blob, BlobHeader } from './fileformat';
 import { HeaderBlock, PrimitiveBlock } from './osmformat';
 import Pbf = require('pbf');
 import { unzlibSync } from 'fflate';
@@ -8,17 +8,17 @@ export const WAY = 1;
 export const RELATION = 2;
 
 interface OsmBlock {
-    type: 'OSMHeader' | 'OSMData',
-    data: Uint8Array
+    type: 'OSMHeader' | 'OSMData';
+    data: Uint8Array;
 }
 
-export async function* parse (input: ReadableStream<Uint8Array>) {
+export async function* parse(input: ReadableStream<Uint8Array>) {
     yield* parseOsmBlock(parseChunk(bufferGenerator(input)));
 }
 
-async function* bufferGenerator (input: ReadableStream<Uint8Array>): AsyncGenerator<ArrayBuffer> {
+async function* bufferGenerator(input: ReadableStream<Uint8Array>): AsyncGenerator<ArrayBuffer> {
     const reader = input.getReader();
-   
+
     while (true) {
         const { done, value } = await reader.read();
         if (done) {
@@ -32,7 +32,10 @@ const LENGTH = 0;
 const HEADER = 1;
 const BLOB = 2;
 
-async function* parseChunk(chunks: AsyncGenerator<ArrayBuffer>, debug = false): AsyncGenerator<OsmBlock> {
+async function* parseChunk(
+    chunks: AsyncGenerator<ArrayBuffer>,
+    debug = false,
+): AsyncGenerator<OsmBlock> {
     let pbf: Pbf | null = null;
     let state: number = LENGTH;
     let waitSize: number = 4;
@@ -78,23 +81,23 @@ async function* parseChunk(chunks: AsyncGenerator<ArrayBuffer>, debug = false): 
                 const blob = Blob.read(pbf, pbf.pos + waitSize);
                 yield {
                     type: header.type,
-                    data: unzlibSync(blob.zlib_data)
+                    data: unzlibSync(blob.zlib_data),
                 };
                 state = LENGTH;
                 waitSize = 4;
             }
-        }    
+        }
     }
 }
 
-async function* parseOsmBlock (blocks: AsyncGenerator<OsmBlock>) {
+async function* parseOsmBlock(blocks: AsyncGenerator<OsmBlock>) {
     for await (const raw of blocks) {
         const pbf = new Pbf(raw.data);
         if (raw.type === 'OSMHeader') {
             const block = HeaderBlock.read(pbf);
-            yield []
+            yield [];
         } else if (raw.type === 'OSMData') {
-            const block = PrimitiveBlock.read(pbf); 
+            const block = PrimitiveBlock.read(pbf);
             yield decodeBlock(block);
         }
         pbf.destroy();
@@ -103,24 +106,24 @@ async function* parseOsmBlock (blocks: AsyncGenerator<OsmBlock>) {
 
 function decodeBlock(block: any) {
     const strings = block.stringtable.s;
-    const result: any[] = []
+    const result: any[] = [];
 
     let { lat_offset, lon_offset, granularity } = block;
     if (!granularity) {
         granularity = 100;
     }
 
-    const getLat = lat => .000000001 * (lat_offset + (granularity * lat));
-    const getLon = lon => .000000001 * (lon_offset + (granularity * lon))
+    const getLat = (lat) => 0.000000001 * (lat_offset + granularity * lat);
+    const getLon = (lon) => 0.000000001 * (lon_offset + granularity * lon);
 
     for (const group of block.primitivegroup) {
         for (const node of group.nodes) {
             console.log(node);
-            throw new Error('Unpacking sparse node not implemented')
+            throw new Error('Unpacking sparse node not implemented');
         }
         for (const way of group.ways) {
-            const tags = {}
-            for (let i = 0; i < way.keys.length; i ++) {
+            const tags = {};
+            for (let i = 0; i < way.keys.length; i++) {
                 tags[strings[way.keys[i]]] = strings[way.vals[i]];
             }
             const refs: number[] = [];
@@ -129,16 +132,16 @@ function decodeBlock(block: any) {
                 r += way.refs[i];
                 refs.push(r);
             }
-            result.push({ 
-                type: WAY, 
+            result.push({
+                type: WAY,
                 id: way.id,
                 tags,
-                refs
+                refs,
             });
         }
         for (const rel of group.relations) {
-            const tags = {}
-            for (let i = 0; i < rel.keys.length; i ++) {
+            const tags = {};
+            for (let i = 0; i < rel.keys.length; i++) {
                 tags[strings[rel.keys[i]]] = strings[rel.vals[i]];
             }
             const members: any[] = [];
@@ -146,8 +149,8 @@ function decodeBlock(block: any) {
                 members.push({
                     id: rel.memids[i],
                     role: rel.roles_sid[i],
-                    type: rel.types[i]
-                })
+                    type: rel.types[i],
+                });
             }
             result.push({
                 type: RELATION,
@@ -162,7 +165,7 @@ function decodeBlock(block: any) {
             let lat = 0;
             let lon = 0;
             for (let i = 0; i < dense.id.length; i++) {
-                const tags = {}
+                const tags = {};
                 const kv = dense.keys_vals;
                 while (kv[tpos] !== 0) {
                     tags[strings[kv[tpos]]] = strings[kv[tpos + 1]];
@@ -175,8 +178,8 @@ function decodeBlock(block: any) {
                     type: NODE,
                     lat: getLat(lat),
                     lon: getLon(lon),
-                    tags
-                }) 
+                    tags,
+                });
             }
         }
     }
@@ -188,8 +191,8 @@ function concatArrayBuffers(buffer1, buffer2) {
     tmp.set(new Uint8Array(buffer1), 0);
     tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
     return tmp.buffer;
-};
+}
 
 function readLength(arr: Uint8Array, pos: number) {
-    return arr[pos] << 24 | arr[pos + 1] << 16 | arr[pos + 2] << 8 | arr[pos + 3];
+    return (arr[pos] << 24) | (arr[pos + 1] << 16) | (arr[pos + 2] << 8) | arr[pos + 3];
 }
